598548657b4aadc1327134e3c0da247bf2e08547
Musashi <musashidelacruz@gmail.com>
2024-10-25 13:42:13 -1000

    modified:   index.html 	renamed:    justincase.js -> script.js 	renamed:    websitecopy.json -> text.json

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('data.json') // Replace 'data.json' with the actual path to your JSON file
+    fetch('text.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -12,10 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 const element = document.querySelector(`[data-key="${key}"]`);
                 if (element) {
                     element.textContent = textArea[key];
-                }
             }
         })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
         });
-});
+})
